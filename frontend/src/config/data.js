98d7eaf9c804ffab3e44f0250419d4317b92157f
Hammad Ali <hammad19ali@gmail.com>
2026-06-18 12:05:15 -0700

    update /api

--- frontend/src/config/data.js
+++ frontend/src/config/data.js
@@ -1,5 +1,1 @@
-export const API_BASE = typeof window !== 'undefined' && 
-  (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') && 
-  window.location.port !== '6947'
-    ? 'http://localhost:6947/api'
-    : '/api';
+export const API_BASE = '/api';
